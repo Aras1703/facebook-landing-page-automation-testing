1774843d3f9a26bad3f6eac0e470c9ce9efe10da
Arasy Bazwir <83108505+Aras1703@users.noreply.github.com>
2023-05-03 20:02:29 +0700

    Update constants.py

--- facebook/constants.py
+++ facebook/constants.py
@@ -1,4 +1,1 @@
 BASE_URL = "https://web.facebook.com/"
-credential = {"email":" ",# put your email
-              "phone":" ",# put your phone number
-              "password":" "}# put your password
